e5e9168054e130461756280bf5fa4d23e06b6fe9
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-05-14 14:09:40 +0000

    add-task-file

---
+++ TransfomerTask/Transformer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TransformerTask
+{
+    /// <summary>
+    /// Implement transformer class.
+    /// </summary>
+    public class Transformer
+    {
+        /// <summary>
+        /// Transform each element of source array into its "word format".
+        /// </summary>
+        /// <param name="source">Source array.</param>
+        /// <returns>Array of "word format" of elements of source array.</returns>
+        /// <exception cref="ArgumentNullException">Throw if array is null.</exception>
+        /// <exception cref="ArgumentException">Throw if array is empty.</exception>
+        public string[] Transform(double[] source)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
