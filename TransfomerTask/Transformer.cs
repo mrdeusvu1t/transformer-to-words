3913e83982ca96d578f8bdcb4c3bd7a88d3e5662
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-11-02 13:02:54 +0000

    Update Transformer.cs

--- TransfomerTask/Transformer.cs
+++ TransfomerTask/Transformer.cs
@@ -3,9 +3,9 @@ using System;
 namespace TransformerTask
 {
     /// <summary>
-    /// Implement transformer class.
+    /// Implements transformer class.
     /// </summary>
-    public sealed class Transformer
+    public class Transformer
     {
         /// <summary>
         /// Transforms each element of source array into its 'word format'.
@@ -14,6 +14,9 @@ namespace TransformerTask
         /// <returns>Array of 'word format' of elements of source array.</returns>
         /// <exception cref="ArgumentNullException">Thrown when array is null.</exception>
         /// <exception cref="ArgumentException">Thrown when array is empty.</exception>
+        /// <example>
+        /// new[] { 2.345, -0.0d, 0.0d, 0.1d } => { "Two point three four five", "Minus zero", "Zero", "Zero point one" }.
+        /// </example>
         public string[] Transform(double[] source)
         {
             throw new NotImplementedException("You need to implement this method.");
